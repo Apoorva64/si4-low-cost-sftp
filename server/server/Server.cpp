faa83c2164e1a5cdab544b86fa736f0364979f6f
Apoorva Srinivas Appadoo <appadooapoorva@gmail.com>
2023-12-24 07:47:34 +0100

    feat add logger to server

--- server/server/Server.cpp
+++ server/server/Server.cpp
@@ -7,12 +7,14 @@
 #include <filesystem>
 #include "Server.h"
 #include "OpenSSL.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
 
 #define SPERATOR '|'
 #define FILES_FOLDER "files"
 
 
 Server::Server(int inPort1, int outPort) : SocketCommunication(inPort1, outPort) {
+    this->logger = spdlog::stdout_color_mt("Server");
     // create files folder if not exists
     std::filesystem::create_directory(FILES_FOLDER);
 
@@ -31,40 +33,40 @@ void Server::handleMessage(const std::string& msg) const {
         std::filesystem::path path(filenameStr);
         std::string filename = path.filename();
         std::string file = args.substr(args.find(SPERATOR) + 1);
-        std::cout << "Uploading file: " << filename << std::endl;
+        logger->info("Uploading file: {}", filename);
         std::ofstream outfile(std::string(FILES_FOLDER) + "/" + filename);
         outfile << file;
         outfile.close();
-        std::cout << "File uploaded!" << std::endl;
+        logger->info("File uploaded!");
     } else if (command == "download") {
         const std::string& filename = args;
-        std::cout << "Downloading file: " << filename << std::endl;
+        logger->info("Downloading file: {}", filename);
         std::ifstream file(std::string(FILES_FOLDER) + "/" + filename, std::ios::binary);
         if (!file.is_open()) {
-            std::cout << "File not found" << std::endl;
+            logger->error("File not found");
             return;
         }
         file.seekg(0, std::ios::end);
         unsigned long fileSize = file.tellg();
         file.seekg(0, std::ios::beg);
-        std::cout << "File size: " << fileSize << std::endl;
+        logger->info("File size: {}", fileSize);
         std::string fileContents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-        std::cout << "File contents: " << fileContents << std::endl;
+        logger->info("Sending file contents...");
         this->send(fileContents);
-        std::cout << "File contents sent!" << std::endl;
+        logger->info("File sent!");
     } else if (command == "list") {
-        std::cout << "Listing files..." << std::endl;
+        logger->info("Listing files...");
         std::string files;
         for (const auto & entry : std::filesystem::directory_iterator(FILES_FOLDER)) {
             files += entry.path().filename().string() + SPERATOR;
         }
-        std::cout << "Sending files..." << std::endl;
+        logger->info("Sending files...");
         this->send(files);
-        std::cout << "Files sent!" << std::endl;
+        logger->info("Files sent!");
     }
 
     else {
-        std::cout << "Unknown command: " << command << std::endl;
+        logger->error("Unknown command: {}", command);
     }
 
 }
